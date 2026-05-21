678b6250ce910717b10dd92746970ece2fcf63be
Christopher Cho <107394328+ChrisC920@users.noreply.github.com>
2026-05-21 00:34:53 -0400

    Add: buy-and-sell-crypto-with-cooldown - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # dp[i][1] -> max profit starting at day i when we are allowed to buy
+        # dp[i][0] -> max profit starting at day i when we are holding a stock (sell)
+        dp = [[0] * 2 for _ in range(len(prices) + 1)]
+
+        for i in range(len(prices) - 1, -1, -1):
+            # buy
+            buy = dp[i + 1][0] - prices[i] if i < len(prices) - 1 else -prices[i]
+            wait = dp[i + 1][1] if i < len(prices) - 1 else 0
+            dp[i][1] = max(buy, wait)
+
+            # sell
+            sell = dp[i + 2][1] + prices[i] if i < len(prices) - 2 else prices[i]
+            wait = dp[i + 1][0] if i < len(prices) - 1 else 0
+            dp[i][0] = max(sell, wait)
+
+        return dp[0][1]
